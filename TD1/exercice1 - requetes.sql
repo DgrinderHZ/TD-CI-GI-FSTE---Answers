34652355192900ba7af44d956f494ac4be9bc7df
DgrinderHZ <hassanzekkouri@gmail.com>
2024-04-19 10:06:47 +0100

    Fin TP1

--- TD1/exercice1 - requetes.sql
+++ TD1/exercice1 - requetes.sql
@@ -1,44 +1,44 @@
 --  (a) Donnez la liste des noms des pilotes :
 SELECT NOMPIL FROM PILOTE;
 
---  (b) Donnez la liste des villes desservies (arrivÃ©e ou dÃ©part) :
+--  (b) Donnez la liste des villes desservies (arrivée ou départ) :
 SELECT DISTINCT VILLE_DEP AS VILLES FROM VOL
 UNION
 SELECT DISTINCT VILLE_ARR FROM VOL;
 
---  (c) Donnez la liste des villes qui sont desservies en arrivÃ©e et en dÃ©part :
+--  (c) Donnez la liste des villes qui sont desservies en arrivée et en départ :
 SELECT VILLE_DEP AS VILLES
 FROM VOL
 INTERSECT
 SELECT VILLE_ARR
 FROM VOL;
 
---  (d) Donnez la liste des villes qui sont desservies uniquement en dÃ©part :
+--  (d) Donnez la liste des villes qui sont desservies uniquement en départ :
 SELECT DISTINCT VILLE_DEP AS VILLES
 FROM VOL
 WHERE VILLE_DEP NOT IN (SELECT VILLE_ARR FROM VOL);
 
---  (e) Donnez la liste des avions (leurs numÃ©ros) dont la capacitÃ© est supÃ©rieure Ã  350 passagers :
+--  (e) Donnez la liste des avions (leurs numéros) dont la capacité est supérieure à  350 passagers :
 SELECT NUMAV
 FROM AVION
 WHERE CAPACITE > 350;
 
---  (f) Quels sont les numÃ©ros et noms des avions localisÃ©s Ã  Nice ?
+--  (f) Quels sont les numéros et noms des avions localisés à  Nice ?
 SELECT NUMAV, NOMAV
 FROM AVION
 WHERE LOC = 'Nice';
 
---  (g) Quels sont les numÃ©ros des pilotes en service et les villes de dÃ©part de leurs vols ?
+--  (g) Quels sont les numéros des pilotes en service et les villes de départ de leurs vols ?
 SELECT DISTINCT P.NUMPIL, V.VILLE_DEP
 FROM PILOTE P
 JOIN VOL V ON P.NUMPIL = V.NUMPIL;
 
---  (h) Quel est le nom des pilotes domiciliÃ©s Ã  Paris dont le salaire est supÃ©rieur Ã  2500 euros ?
+--  (h) Quel est le nom des pilotes domiciliés à  Paris dont le salaire est supérieur à  2500 euros ?
 SELECT NOMPIL
 FROM PILOTE
 WHERE ADR = 'Paris' AND SAL > 2500;
 
---  (i) Donnez les numÃ©ros et nom des pilotes homonymes (mÃªme nom) :
+--  (i) Donnez les numéros et nom des pilotes homonymes (même nom) :
 SELECT NUMPIL, NOMPIL
 FROM PILOTE 
 WHERE NOMPIL IN (
@@ -48,46 +48,46 @@ WHERE NOMPIL IN (
     HAVING COUNT(*) > 1
 );
 
---  (j) Quels sont les numÃ©ros des pilotes qui ne sont pas en service ?
+--  (j) Quels sont les numéros des pilotes qui ne sont pas en service ?
 SELECT NUMPIL
 FROM PILOTE
 WHERE NUMPIL NOT IN (SELECT DISTINCT NUMPIL FROM VOL);
 
---  (k) Quels sont les vols (numÃ©ro, ville de dÃ©part) effectuÃ©s par les pilotes de numÃ©ro 100 et 204 ?
+--  (k) Quels sont les vols (numéro, ville de départ) effectués par les pilotes de numéro 100 et 204 ?
 SELECT NUMVOL, VILLE_DEP
 FROM VOL
 WHERE NUMPIL IN (100, 204);
 
---  (l) Quels sont les numÃ©ros des pilotes en service qui ne s'appellent pas Durand ?
+--  (l) Quels sont les numéros des pilotes en service qui ne s'appellent pas Durand ?
 SELECT NUMPIL
 FROM VOL
 WHERE NUMPIL IN (SELECT NUMPIL FROM PILOTE WHERE NOMPIL != 'Durand');
 
---  (m) Donnez le numÃ©ro des vols effectuÃ©s au dÃ©part de Nice par des pilotes NiÃ§ois ?
+--  (m) Donnez le numéro des vols effectués au départ de Nice par des pilotes Niçois ?
 SELECT NUMVOL
 FROM VOL
 WHERE VILLE_DEP = 'Nice' AND NUMPIL IN (SELECT NUMPIL FROM PILOTE WHERE ADR = 'Nice');
 
---  (n) Quels sont les vols effectuÃ©s par un avion qui n'est pas localisÃ© Ã  Nice ?
+--  (n) Quels sont les vols effectués par un avion qui n'est pas localisé à  Nice ?
 SELECT NUMVOL
 FROM VOL
 WHERE NUMAV NOT IN (SELECT NUMAV FROM AVION WHERE LOC = 'Nice');
 
---  (o) Quels sont les pilotes (numÃ©ro et nom) assurant au moins un vol au dÃ©part de Nice avec un avion de capacitÃ© supÃ©rieure Ã  300 places ?
+--  (o) Quels sont les pilotes (numéro et nom) assurant au moins un vol au départ de Nice avec un avion de capacité supérieure à  300 places ?
 SELECT DISTINCT P.NUMPIL, P.NOMPIL
 FROM PILOTE P
 JOIN VOL V ON P.NUMPIL = V.NUMPIL
 JOIN AVION A ON V.NUMAV = A.NUMAV
 WHERE V.VILLE_DEP = 'Nice' AND A.CAPACITE > 300;
 
---  (p) Quels sont les noms des pilotes domiciliÃ©s Ã  Paris assurant un vol au dÃ©part de Nice avec un Airbus A380 ?
+--  (p) Quels sont les noms des pilotes domiciliés à  Paris assurant un vol au départ de Nice avec un Airbus A380 ?
 SELECT DISTINCT P.NOMPIL
 FROM PILOTE P
 JOIN VOL V ON P.NUMPIL = V.NUMPIL
 JOIN AVION A ON V.NUMAV = A.NUMAV
 WHERE P.ADR = 'Paris' AND V.VILLE_DEP = 'Nice' AND A.NOMAV = 'Airbus A380';
 
---  (q) Quels sont les numÃ©ros des vols effectuÃ©s par un pilote NiÃ§ois au dÃ©part ou Ã  l'arrivÃ©e de Nice avec un avion localisÃ© Ã  Paris ?
+--  (q) Quels sont les numéros des vols effectués par un pilote Niçois au départ ou à  l'arrivée de Nice avec un avion localisé à  Paris ?
 SELECT DISTINCT V.NUMVOL
 FROM VOL V
 JOIN PILOTE P ON V.NUMPIL = P.NUMPIL
@@ -95,22 +95,22 @@ JOIN AVION A ON V.NUMAV = A.NUMAV
 WHERE (V.VILLE_DEP = 'Nice' OR V.VILLE_ARR = 'Nice')
 AND P.ADR = 'Nice' AND A.LOC = 'Paris';
 
---  (r) Quels sont les pilotes (numÃ©ro et nom) habitant dans la mÃªme ville que le pilote Dupont (on suppose qu'il n'y en a qu'un !) ?
+--  (r) Quels sont les pilotes (numéro et nom) habitant dans la même ville que le pilote Dupont (on suppose qu'il n'y en a qu'un !) ?
 SELECT NUMPIL, NOMPIL
 FROM PILOTE
 WHERE ADR = (SELECT ADR FROM PILOTE WHERE NOMPIL = 'Dupont');
 
---  (s) Quelles sont les villes desservies Ã  partir de la ville d'arrivÃ©e d'un vol au dÃ©part de Paris ?
+--  (s) Quelles sont les villes desservies à  partir de la ville d'arrivée d'un vol au départ de Paris ?
 SELECT DISTINCT V.VILLE_ARR
 FROM VOL V
 WHERE V.VILLE_DEP = 'Paris';
 
---  (t) Quels sont les appareils (leur numÃ©ro) localisÃ©s dans la mÃªme ville que l'avion numÃ©ro 100 ?
+--  (t) Quels sont les appareils (leur numéro) localisés dans la même ville que l'avion numéro 100 ?
 SELECT NUMAV
 FROM AVION
 WHERE LOC = (SELECT LOC FROM AVION WHERE NUMAV = 100);
 
---  (u) Quels sont les numÃ©ros et noms des pilotes qui effectuent un vol au dÃ©part de leur ville de rÃ©sidence ?
+--  (u) Quels sont les numéros et noms des pilotes qui effectuent un vol au départ de leur ville de résidence ?
 SELECT P.NUMPIL, P.NOMPIL
 FROM PILOTE P
 JOIN VOL V ON P.NUMPIL = V.NUMPIL
--- TD1/exercice2 - queries.sql
+++ TD1/exercice2 - queries.sql
@@ -1,24 +1,24 @@
--- 1. La rÃ©fÃ©rence, le nom et la ville de toutes les usines de Rabat
+-- 1. La référence, le nom et la ville de toutes les usines de Rabat
 SELECT ref_usine, nom_usine, ville
 FROM usine
 WHERE ville = 'Rabat';
 
--- 2. Les rÃ©fÃ©rences des magasins qui sont approvisionnÃ©s (alimentÃ©s) par lâ€™usine de rÃ©fÃ©rence 101 en produit de rÃ©fÃ©rence 1
+-- 2. Les références des magasins qui sont approvisionnés (alimentés) par l'usine de référence 101 en produit de référence 1
 SELECT ref_mag
 FROM provenance
 WHERE ref_usine = 101 AND ref_prod = 1;
 
--- 3. La rÃ©fÃ©rence et le nom de tous les produits rouges
+-- 3. La référence et le nom de tous les produits rouges
 SELECT ref_prod, nom_prod
 FROM produit
 WHERE couleur = 'rouge';
 
--- 4. La rÃ©fÃ©rence et le nom de tous les produits dont le nom commence par "Produit"
+-- 4. La référence et le nom de tous les produits dont le nom commence par "Produit"
 SELECT ref_prod, nom_prod
 FROM produit
 WHERE nom_prod LIKE 'Produit%';
 
--- 5. La rÃ©fÃ©rence des magasins auxquels on livre quelque chose. Faites la requÃªte sans prÃ©ciser DISTINCT puis avec
+-- 5. La référence des magasins auxquels on livre quelque chose. Faites la requête sans préciser DISTINCT puis avec
 -- Sans DISTINCT
 SELECT ref_mag
 FROM provenance;
@@ -27,15 +27,15 @@ FROM provenance;
 SELECT DISTINCT ref_mag
 FROM provenance;
 
--- 6. La rÃ©fÃ©rence des magasins auxquels on ne livre rien (utilisez EXCEPT)
+-- 6. La référence des magasins auxquels on ne livre rien (utilisez EXCEPT)
 SELECT ref_mag
 FROM magasin
 EXCEPT
 SELECT ref_mag
 FROM provenance;
 
--- 7. Le nom et la couleur des produits livrÃ©s par lâ€™usine de rÃ©fÃ©rence 103. 
--- Faites la mÃªme requÃªte avec NATURAL JOIN pour ne pas afficher deux fois le mÃªme produit, on peut utiliser DISTINCT ON (ref_prod) mÃªme si ref_prod nâ€™est pas affichÃ©
+-- 7. Le nom et la couleur des produits livrés par l'usine de référence 103. 
+-- Faites la même requête avec NATURAL JOIN pour ne pas afficher deux fois le même produit, on peut utiliser DISTINCT ON (ref_prod) même si ref_prod n'est pas affiché
 -- Avec JOIN
 SELECT DISTINCT p.nom_prod, p.couleur
 FROM provenance pr
@@ -49,34 +49,37 @@ NATURAL JOIN produit p
 WHERE pr.ref_usine = 103;
 
 
--- 8. Les rÃ©fÃ©rences des magasins qui sont approvisionnÃ©s par lâ€™usine de rÃ©fÃ©rence 102 en un produit rouge
+-- 8. Les références des magasins qui sont approvisionnés par l'usine de référence 102 en un produit rouge
 SELECT DISTINCT pr.ref_mag
 FROM provenance pr
 JOIN produit p ON pr.ref_prod = p.ref_prod
 WHERE pr.ref_usine = 102 AND p.couleur = 'rouge';
 
--- 9. Le poids de la livraison (renommÃ© poids_livraisons) en produit de rÃ©fÃ©rence 3 livrÃ©s au magasin 203 par lâ€™usine de rÃ©fÃ©rence 103
+-- 9. Le poids de la livraison (renommé poids_livraisons) en produit de référence 3 livrés au magasin 203 par l'usine de référence 103
 SELECT quantite * p.poids AS poids_livraisons
 FROM provenance pr
 JOIN produit p ON pr.ref_prod = p.ref_prod
 WHERE pr.ref_usine = 103 AND pr.ref_mag = 203 AND pr.ref_prod = 3;
 
--- 10. Pour chaque produit, chaque magasin et chaque usine, le poids de livraison (renommÃ© poids_livraisons), la rÃ©fÃ©rence du produit, celle du magasin et celle de lâ€™usine
+-- 10. Pour chaque produit, chaque magasin et chaque usine, le poids de livraison (renommé poids_livraisons), la référence du produit, celle du magasin et celle de l'usine
+-- Note: Donner le poids de livraison Pour chaque produit, chaque magasin et chaque usine.
 SELECT pr.ref_prod, pr.ref_mag, pr.ref_usine, pr.quantite * p.poids AS poids_livraisons
 FROM provenance pr
 JOIN produit p ON pr.ref_prod = p.ref_prod;
 
--- 11. Les couples nom dâ€™usine, nom de magasin qui sont dans la mÃªme ville avec le nom de la ville
+-- 11. Les couples nom d'usine, nom de magasin qui sont dans la même ville avec le nom de la ville
 SELECT u.nom_usine, m.nom_mag, u.ville
 FROM usine u
 JOIN magasin m ON u.ville = m.ville;
 
--- 12. Les couples de magasins (rÃ©fÃ©rence) qui sâ€™approvisionnent du mÃªme produit fabriquÃ© par la mÃªme usine
+-- 12. Les couples de magasins (référence) qui s'approvisionnent du même produit fabriqué par la même usine
+-- Note: vous pouvez utiliser <>, < ou > pour la partie (p1.ref_mag <> p2.ref_mag)
 SELECT p1.ref_mag, p2.ref_mag
 FROM provenance p1
 JOIN provenance p2 ON p1.ref_prod = p2.ref_prod AND p1.ref_usine = p2.ref_usine AND p1.ref_mag <> p2.ref_mag;
 
--- 13. La mÃªme chose en affichant les noms des magasins (utiliser NATURAL JOIN Ã  bon escient)
+-- 13. La même chose en affichant les noms des magasins (utiliser NATURAL JOIN à  bon escient)
+-- Note: Faites des jointure deux fois avec la table magasin
 SELECT m1.nom_mag, m2.nom_mag
 FROM provenance p1
 NATURAL JOIN provenance p2
@@ -84,29 +87,30 @@ JOIN magasin m1 ON p1.ref_mag = m1.ref_mag
 JOIN magasin m2 ON p2.ref_mag = m2.ref_mag
 WHERE p1.ref_prod = p2.ref_prod AND p1.ref_usine = p2.ref_usine AND p1.ref_mag <> p2.ref_mag;
 
--- 14. Les noms des magasins qui sâ€™approvisionnent en le produit de rÃ©fÃ©rence 12
+-- 14. Les noms des magasins qui s'approvisionnent en le produit de référence 12
 -- Avec une jointure
 SELECT m.nom_mag
 FROM magasin m
 JOIN provenance p ON m.ref_mag = p.ref_mag
 WHERE p.ref_prod = 12;
 
--- Avec une sous-requÃªte
+-- Avec une sous-requête
 SELECT nom_mag
 FROM magasin
 WHERE ref_mag IN (SELECT ref_mag FROM provenance WHERE ref_prod = 1);
 
--- 15. Les noms des magasins qui ne sâ€™approvisionnent pas en le produit de rÃ©fÃ©rence 12 (avec une sous-requÃªte)
+-- 15. Les noms des magasins qui ne s'approvisionnent pas en le produit de référence 12 (avec une sous-requête)
 SELECT nom_mag
 FROM magasin
 WHERE ref_mag NOT IN (SELECT ref_mag FROM provenance WHERE ref_prod = 12);
 
--- 16. Les noms des magasins qui sâ€™approvisionnent en produit rouge (avec des sous-requÃªtes)
+-- 16. Les noms des magasins qui s'approvisionnent en produit rouge (avec des sous-requêtes)
 SELECT nom_mag
 FROM magasin
 WHERE ref_mag IN (SELECT ref_mag FROM provenance JOIN produit ON provenance.ref_prod = produit.ref_prod WHERE couleur = 'rouge');
 
--- 17. Les noms des magasins qui ne sâ€™approvisionnent pas en enfer (nom de 2 usines) (avec des sous-requÃªtes)
+-- 17. Les noms des magasins qui ne s'approvisionnent pas en enfer (nom de 2 usines) (avec des sous-requêtes)
+-- Note: Ici on considère deux usines d'approvisionment en 'enfer' (enfer1, enfer2)
 SELECT nom_mag
 FROM magasin
 WHERE ref_mag NOT IN (
